30a64f44a623e32c2b9636fe8c75c5538d3782c4
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-09-21 14:15:13 +0000

    broo add he gemini keys in env and use them plase   apiKey: "AIzaSyCzALp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCzALpUYx4X7xXrX1OXy-NRVDtdA_b16Q8",
+  authDomain: "surebet-d7d10.firebaseapp.com",
+  projectId: "surebet-d7d10",
+  storageBucket: "surebet-d7d10.appspot.com",
+  messagingSenderId: "590360869636",
+  appId: "1:590360869636:web:fc8eeb556c6052d4926e51",
+  measurementId: "G-B2FG7XDY03"
 };
 
 // Initialize Firebase
